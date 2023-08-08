80d0cd2356a15270a5bd5f7d7593024a14188475
Alpha Olomi <alphaolomi@gmail.com>
2023-08-08 13:21:11 +0300

    Update CounterTest.php

--- tests/Feature/Livewire/CounterTest.php
+++ tests/Feature/Livewire/CounterTest.php
@@ -3,15 +3,34 @@
 use App\Livewire\Counter;
 
 use function Pest\Livewire\livewire;
+
+it('starts counter at 1', function () {
+    $counter = livewire(Counter::class);
+
+    expect($counter->count)->toBe(1);
+});
  
+
 it('can be incremented', function () {
-    livewire(Counter::class)
-        ->call('increment')
-        ->assertSee(1);
+    // Arrange
+    $counter = livewire(Counter::class);
+    
+    // Act
+    $counter->call('increment');
+
+    // Assert
+    expect($counter->count)->toBe(2);
+    $counter->assertSee(2);
 });
  
 it('can be decremented', function () {
-    livewire(Counter::class)
-        ->call('decrement')
-        ->assertSee(-1);
+    // Arrange
+    $counter = livewire(Counter::class);
+    
+    // Act
+    $counter->call('decrement');
+
+    // Assert
+    expect($counter->count)->toBe(0);
+    $counter->assertSee(0);
 });
