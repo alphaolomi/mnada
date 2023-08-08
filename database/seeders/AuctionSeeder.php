8d1b240bee6e7f3a659b53926682d0bf8b34b917
Alpha Olomi <alphaolomi@gmail.com>
2023-08-08 13:11:42 +0300

    update seeder

--- database/seeders/AuctionSeeder.php
+++ database/seeders/AuctionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AuctionSeeder extends Seeder
 {
@@ -13,14 +14,30 @@ class AuctionSeeder extends Seeder
      */
     public function run(): void
     {
+        try {
+            DB::beginTransaction();
+            $user = User::first();
 
-        \App\Models\Auction::factory()->create([
-            'title' => 'Test Auction',
-            'description' => 'This is a test auction.',
-            'is_published' => true,
-            'start_time' => now(),
-            'end_time' => now()->addDays(7),
-            'seller_id' => User::first()->id,
-        ]);
+            $auction  = \App\Models\Auction::factory()->create([
+                'title' => 'Test Auction',
+                'description' => 'This is a test auction.',
+                'is_published' => true,
+                'start_time' => now(),
+                'end_time' => now()->addDays(7),
+                'seller_id' => $user->id,
+            ]);
+
+            $auction->items()->saveMany(
+                \App\Models\AuctionItem::factory()
+                    ->count(2)
+                    ->state(['seller_id' => $user->id])
+                    ->make()
+            );
+            DB::commit();
+            echo 'Auction seeded successfully.';
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            echo $th->getMessage();
+        }
     }
 }
