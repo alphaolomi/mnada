12feceac66f57d0878c00103292d598bee633099
Alpha Olomi <alphaolomi@gmail.com>
2023-08-13 14:20:47 +0300

    add Auction LV component

---
+++ app/Livewire/Auction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Auction extends Component
+{
+    public function render()
+    {
+        return view('livewire.auction');
+    }
+}
