29a1de534d1977df6f2c8bf7c18cb8b0fe22f5dc
Alpha Olomi <alphaolomi@gmail.com>
2023-08-13 17:21:31 +0300

    Create level-up.php

---
+++ config/level-up.php
@@ -0,0 +1,77 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | User Foreign Key
+    |--------------------------------------------------------------------------
+    |
+    | This value is the foreign key that will be used to relate the Experience model to the User model.
+    |
+     */
+    'user' => [
+        'foreign_key' => 'user_id',
+        'model' => App\Models\User::class,
+        'users_table' => 'users',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Experience Table
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of the table that will be used to store experience data.
+    |
+     */
+    'table' => 'experiences',
+
+    /*
+    |-----------------------------------------------------------------------
+    | Starting Level
+    |-----------------------------------------------------------------------
+    |
+    | The level that a User starts with.
+    |
+    */
+    'starting_level' => 1,
+
+    /*
+    |-----------------------------------------------------------------------
+    | Multiplier Paths
+    |-----------------------------------------------------------------------
+    |
+    | Set the path and namespace for the Multiplier classes.
+    |
+    */
+    'multiplier' => [
+        'enabled' => env(key: 'MULTIPLIER_ENABLED', default: true),
+        'path' => env(key: 'MULTIPLIER_PATH', default: app_path(path: 'Multipliers')),
+        'namespace' => env(key: 'MULTIPLIER_NAMESPACE', default: 'App\\Multipliers\\'),
+    ],
+
+    /*
+    |-----------------------------------------------------------------------
+    | Level Cap
+    |-----------------------------------------------------------------------
+    |
+    | Set the maximum level a User can reach.
+    |
+    */
+    'level_cap' => [
+        'enabled' => env(key: 'LEVEL_CAP_ENABLED', default: true),
+        'level' => env(key: 'LEVEL_CAP', default: 100),
+        'points_continue' => env(key: 'LEVEL_CAP_POINTS_CONTINUE', default: true),
+    ],
+
+    /*
+    | -------------------------------------------------------------------------
+    | Audit
+    | -------------------------------------------------------------------------
+    |
+    | Set the audit configuration.
+    |
+    */
+    'audit' => [
+        'enabled' => env(key: 'AUDIT_POINTS', default: false),
+    ],
+];
