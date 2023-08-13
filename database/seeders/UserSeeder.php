5faead1687caf448c036923c2ca998ef59496c11
Alpha Olomi <alphaolomi@gmail.com>
2023-08-13 14:58:39 +0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,11 +13,15 @@ final class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        try {
+            \App\Models\User::factory(10_000)->create();
 
-        \App\Models\User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            \App\Models\User::factory()->create([
+                'name'  => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
