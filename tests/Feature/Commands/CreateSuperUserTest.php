2f16e26ba38909bc48ef6d7f742b106d01f7d60e
Alpha Olomi <alphaolomi@gmail.com>
2023-08-15 11:59:42 +0300

    Create CreateSuperUserTest.php

---
+++ tests/Feature/Commands/CreateSuperUserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+
+it('lists throwable classes', function () {
+    Artisan::call('dev:list-throwable-classes-command');
+
+    // Your assertions here based on the expected behavior of ListThrowableClassesCommand.
+    // For example, you might want to check if certain classes are listed.
+    // You can also assert the output or any other effects of running the command.
+    
+    // For instance:
+    $output = Artisan::output();
+    // expect($output)->toContain('SomeThrowableClass');
+    // expect($output)->toContain('AnotherThrowableClass');
+    
+    // You can add more assertions as needed.
+});
